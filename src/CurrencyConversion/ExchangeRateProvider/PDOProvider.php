14a34e3574b09dedf21070b9aee1549f35e36074
Benjamin Morel <benjamin.morel@gmail.com>
2015-07-09 22:31:54 +0200

    PDOProvider now uses a configuration object
    This allows autocompletion and static code analysis.

--- src/CurrencyConversion/ExchangeRateProvider/PDOProvider.php
+++ src/CurrencyConversion/ExchangeRateProvider/PDOProvider.php
@@ -11,46 +11,24 @@ use Brick\Money\Exception\CurrencyConversionException;
  */
 class PDOProvider implements ExchangeRateProvider
 {
-    const TABLE_NAME                  = 'TABLE_NAME';
-    const EXCHANGE_RATE_COLUMN_NAME   = 'EXCHANGE_RATE_COLUMN_NAME';
-    const SOURCE_CURRENCY_COLUMN_NAME = 'SOURCE_CURRENCY_COLUMN_NAME';
-    const TARGET_CURRENCY_COLUMN_NAME = 'TARGET_CURRENCY_COLUMN_NAME';
-
     /**
      * @var \PDOStatement
      */
     private $statement;
 
     /**
-     * @param \PDO  $pdo
-     * @param array $configuration
-     *
-     * @throws \InvalidArgumentException
+     * @param \PDO                     $pdo
+     * @param PDOProviderConfiguration $configuration
      */
-    public function __construct(\PDO $pdo, array $configuration)
+    public function __construct(\PDO $pdo, PDOProviderConfiguration $configuration)
     {
-        $keys = [
-            self::TABLE_NAME,
-            self::EXCHANGE_RATE_COLUMN_NAME,
-            self::SOURCE_CURRENCY_COLUMN_NAME,
-            self::TARGET_CURRENCY_COLUMN_NAME
-        ];
-
-        foreach ($keys as $key) {
-            if (! isset($configuration[$key])) {
-                throw new \InvalidArgumentException($key . ' is not configured.');
-            }
-        }
-
-        $query = sprintf(
+        $this->statement = $pdo->prepare(sprintf(
             'SELECT %s FROM %s WHERE %s = ? AND %s = ?',
-            $configuration[self::EXCHANGE_RATE_COLUMN_NAME],
-            $configuration[self::TABLE_NAME],
-            $configuration[self::SOURCE_CURRENCY_COLUMN_NAME],
-            $configuration[self::TARGET_CURRENCY_COLUMN_NAME]
-        );
-
-        $this->statement = $pdo->prepare($query);
+            $configuration->exchangeRateColumnName,
+            $configuration->tableName,
+            $configuration->sourceCurrencyColumnName,
+            $configuration->targetCurrencyColumnName
+        ));
     }
 
     /**
---
+++ src/CurrencyConversion/ExchangeRateProvider/PDOProviderConfiguration.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Brick\Money\CurrencyConversion\ExchangeRateProvider;
+
+/**
+ * Configuration for the PDOProvider.
+ */
+class PDOProviderConfiguration
+{
+    /**
+     * The name of the table that holds the exchange rates.
+     *
+     * @var string
+     */
+    public $tableName;
+
+    /**
+     * The name of the column that holds the source currency code.
+     *
+     * @var string
+     */
+    public $sourceCurrencyColumnName;
+
+    /**
+     * The name of the column that holds the target currency code.
+     *
+     * @var string
+     */
+    public $targetCurrencyColumnName;
+
+    /**
+     * The name of the column that holds the exchange rate for the currency pair.
+     *
+     * @var string
+     */
+    public $exchangeRateColumnName;
+}
