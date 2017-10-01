23f64acb7bb492c790b7592bec868b093af3e7a2
Benjamin Morel <benjamin.morel@gmail.com>
2017-10-01 11:00:39 +0200

    Remove confused wording

--- src/Money.php
+++ src/Money.php
@@ -299,7 +299,7 @@ class Money extends AbstractMoney
     /**
      * Returns the sum of this Money and the given amount.
      *
-     * If the operand is a Money, it must have the same context and scale as this Money, or an exception is thrown.
+     * If the operand is a Money, it must have the same context as this Money, or an exception is thrown.
      * If you do need to add a Money in a different context, you can use `plus($money->getAmount())`.
      *
      * The resulting Money has the same context as this Money. If the result needs rounding to fit this context, a
@@ -325,7 +325,7 @@ class Money extends AbstractMoney
     /**
      * Returns the difference of this Money and the given amount.
      *
-     * If the operand is a Money, it must have the same context and scale as this Money, or an exception is thrown.
+     * If the operand is a Money, it must have the same context as this Money, or an exception is thrown.
      * If you do need to subtract a Money in a different context, you can use `minus($money->getAmount())`.
      *
      * The resulting Money has the same context as this Money. If the result needs rounding to fit this context, a
