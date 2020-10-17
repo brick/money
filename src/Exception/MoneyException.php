ddd9376817e376bec41c735586f38ae70c41d507
Alexey Solodkiy <alexey@solodkiy.by>
2020-10-17 17:49:39 +0300

    reorganize exceptions structure

--- src/Exception/MoneyException.php
+++ src/Exception/MoneyException.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Brick\Money\Exception;
 
-use RuntimeException;
-
 /**
- * Base class for money exceptions.
+ * Interface for money exceptions.
  */
-class MoneyException extends RuntimeException
+interface MoneyException extends \Throwable
 {
 }
