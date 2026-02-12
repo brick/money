b3c4a5aacfea3e79bc5310a38d5c70aecdf9b63f
Benjamin Morel <benjamin.morel@gmail.com>
2026-02-13 00:18:19 +0100

    Add immutable API to MoneyBag

--- src/MoneyBag.php
+++ src/MoneyBag.php
@@ -6,8 +6,12 @@ namespace Brick\Money;
 
 use Brick\Math\BigRational;
 use Override;
+use ReflectionClass;
 
 use function array_values;
+use function trigger_error;
+
+use const E_USER_DEPRECATED;
 
 /**
  * Container for monies in different currencies.
@@ -22,6 +26,39 @@ final class MoneyBag implements Monetary
      * @var array<string, RationalMoney>
      */
     private array $monies = [];
+
+    public function __construct()
+    {
+        trigger_error(
+            'Instantiating a MoneyBag with new is deprecated, and will be disallowed in a future version. ' .
+            'Use MoneyBag::zero(), or create from a list of monies using MoneyBag::fromMonies().',
+            E_USER_DEPRECATED,
+        );
+    }
+
+    /**
+     * Returns an empty MoneyBag of zero value.
+     */
+    public static function zero(): MoneyBag
+    {
+        // Temporary fix to bypass the deprecation warning in the constructor.
+        // This will be removed in a future version.
+        return (new ReflectionClass(MoneyBag::class))->newInstanceWithoutConstructor();
+    }
+
+    /**
+     * Creates a MoneyBag from a list of monies.
+     */
+    public static function fromMonies(Monetary ...$monies): MoneyBag
+    {
+        $moneyBag = MoneyBag::zero();
+
+        foreach ($monies as $money) {
+            $moneyBag = $moneyBag->plus($money);
+        }
+
+        return $moneyBag;
+    }
 
     /**
      * Returns the contained amount in the given currency as a RationalMoney.
@@ -47,7 +84,51 @@ final class MoneyBag implements Monetary
     }
 
     /**
+     * Returns a MoneyBag with the given monetary amount added.
+     *
+     * @param Monetary $money A Money, RationalMoney, or MoneyBag instance.
+     *
+     * @return MoneyBag The new MoneyBag instance.
+     */
+    public function plus(Monetary $money): MoneyBag
+    {
+        $new = clone $this;
+
+        foreach ($money->getMonies() as $containedMoney) {
+            $currency = $containedMoney->getCurrency();
+            $currencyCode = $currency->getCurrencyCode();
+
+            $new->monies[$currencyCode] = $new->getMoney($currency)->plus($containedMoney);
+        }
+
+        return $new;
+    }
+
+    /**
+     * Returns a MoneyBag with the given monetary amount subtracted.
+     *
+     * @param Monetary $money A Money, RationalMoney, or MoneyBag instance.
+     *
+     * @return MoneyBag The new MoneyBag instance.
+     */
+    public function minus(Monetary $money): MoneyBag
+    {
+        $new = clone $this;
+
+        foreach ($money->getMonies() as $containedMoney) {
+            $currency = $containedMoney->getCurrency();
+            $currencyCode = $currency->getCurrencyCode();
+
+            $new->monies[$currencyCode] = $new->getMoney($currency)->minus($containedMoney);
+        }
+
+        return $new;
+    }
+
+    /**
      * Adds money to this bag.
+     *
+     * @deprecated MoneyBag will be made immutable in a future version. Use MoneyBag::plus() instead, which returns a new instance.
      *
      * @param Monetary $money A Money, RationalMoney, or MoneyBag instance.
      *
@@ -55,6 +136,12 @@ final class MoneyBag implements Monetary
      */
     public function add(Monetary $money): MoneyBag
     {
+        trigger_error(
+            'MoneyBag::add() is deprecated, and will be removed in a future version. ' .
+            'MoneyBag will be immutable in the future. Use MoneyBag::plus(), which returns a new instance instead.',
+            E_USER_DEPRECATED,
+        );
+
         foreach ($money->getMonies() as $containedMoney) {
             $currency = $containedMoney->getCurrency();
             $currencyCode = $currency->getCurrencyCode();
@@ -68,12 +155,20 @@ final class MoneyBag implements Monetary
     /**
      * Subtracts money from this bag.
      *
+     * @deprecated MoneyBag will be made immutable in a future version. Use MoneyBag::minus() instead, which returns a new instance.
+     *
      * @param Monetary $money A Money, RationalMoney, or MoneyBag instance.
      *
      * @return MoneyBag This instance.
      */
     public function subtract(Monetary $money): MoneyBag
     {
+        trigger_error(
+            'MoneyBag::subtract() is deprecated, and will be removed in a future version. ' .
+            'MoneyBag will be immutable in the future. Use MoneyBag::minus(), which returns a new instance instead.',
+            E_USER_DEPRECATED,
+        );
+
         foreach ($money->getMonies() as $containedMoney) {
             $currency = $containedMoney->getCurrency();
             $currencyCode = $currency->getCurrencyCode();
--- tests/MoneyBagTest.php
+++ tests/MoneyBagTest.php
@@ -65,4 +65,77 @@ class MoneyBagTest extends AbstractTestCase
             $moneyBag,
         );
     }
+
+    public function testFromMonies(): void
+    {
+        $moneyBag = MoneyBag::fromMonies(
+            Money::of('123', 'EUR'),
+            Money::of('234.99', 'EUR'),
+            Money::of(3, 'JPY'),
+        );
+
+        self::assertMoneyBagContains([Money::of('357.99', 'EUR'), Money::of('3', 'JPY')], $moneyBag);
+    }
+
+    public function testPlusMinusMoney(): MoneyBag
+    {
+        $moneyBag = MoneyBag::zero();
+        self::assertMoneyBagContains([], $moneyBag);
+
+        $moneyBag = $moneyBag->plus(Money::of('123', 'EUR'));
+        self::assertMoneyBagContains([Money::of('123.00', 'EUR')], $moneyBag);
+
+        $moneyBag = $moneyBag->plus(Money::of('234.99', 'EUR'));
+        self::assertMoneyBagContains([Money::of('357.99', 'EUR')], $moneyBag);
+
+        $moneyBag = $moneyBag->plus(Money::of(3, 'JPY'));
+        self::assertMoneyBagContains([Money::of('357.99', 'EUR'), Money::of('3', 'JPY')], $moneyBag);
+
+        $moneyBag = $moneyBag->plus(Money::of('1.1234', 'JPY', new AutoContext()));
+        self::assertMoneyBagContains([Money::of('357.99', 'EUR'), Money::of('4.1234', 'JPY', new AutoContext())], $moneyBag);
+
+        $moneyBag = $moneyBag->minus(Money::of('3.589950', 'EUR', new AutoContext()));
+        self::assertMoneyBagContains([Money::of('354.400050', 'EUR', new AutoContext()), Money::of('4.1234', 'JPY', new AutoContext())], $moneyBag);
+
+        $moneyBag = $moneyBag->plus(RationalMoney::of('1/3', 'EUR'));
+        self::assertMoneyBagContains([RationalMoney::of('21284003/60000', 'EUR'), Money::of('4.1234', 'JPY', new AutoContext())], $moneyBag);
+
+        return $moneyBag;
+    }
+
+    #[Depends('testPlusMinusMoney')]
+    public function testPlusCustomCurrency(MoneyBag $moneyBag): void
+    {
+        $moneyBag = $moneyBag->plus(Money::of('0.1234', new Currency('BTC', 0, 'Bitcoin', 8)));
+        self::assertMoneyBagContains(
+            [
+                RationalMoney::of('21284003/60000', 'EUR'),
+                Money::of('4.1234', 'JPY', new AutoContext()),
+                Money::of('0.1234', new Currency('BTC', 0, 'Bitcoin', 8)),
+            ],
+            $moneyBag,
+        );
+    }
+
+    public function testImmutablePlus(): void
+    {
+        $moneyBag = MoneyBag::zero();
+        $moneyBag = $moneyBag->plus(Money::of(20, 'EUR'));
+
+        $newMoneyBag = $moneyBag->plus(Money::of(5, 'EUR'));
+
+        self::assertMoneyBagContains([Money::of(20, 'EUR')], $moneyBag);
+        self::assertMoneyBagContains([Money::of(25, 'EUR')], $newMoneyBag);
+    }
+
+    public function testImmutableMinus(): void
+    {
+        $moneyBag = MoneyBag::zero();
+        $moneyBag = $moneyBag->plus(Money::of(20, 'EUR'));
+
+        $newMoneyBag = $moneyBag->minus(Money::of(5, 'EUR'));
+
+        self::assertMoneyBagContains([Money::of(20, 'EUR')], $moneyBag);
+        self::assertMoneyBagContains([Money::of(15, 'EUR')], $newMoneyBag);
+    }
 }
