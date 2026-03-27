10208a945be8510fa5ca62ff93bb1bd01cdc0a24
Benjamin Morel <benjamin.morel@gmail.com>
2026-03-27 16:13:10 +0100

    Seal MoneyException

--- src/Exception/MoneyException.php
+++ src/Exception/MoneyException.php
@@ -8,6 +8,11 @@ use Throwable;
 
 /**
  * Interface for money exceptions.
+ *
+ * This interface is sealed: implementing it in userland code is not supported, and breaking changes to this interface
+ * can happen at any time, even in minor or patch releases.
+ *
+ * @phpstan-sealed ContextException|ExchangeRateException|InvalidArgumentException|MoneyFormatException|MoneyMismatchException|UnknownCurrencyException
  */
 interface MoneyException extends Throwable
 {
